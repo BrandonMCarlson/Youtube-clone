6423d462722fb924b4b92d15acc6421805aab053
BrandonMCarlson <Brandoncarlson93bc@gmail.com>
2021-11-17 16:46:35 -0800

    add config and statup folders with correct files. add new infor for models/routes on the comment side.

--- Models/Comment.js
+++ Models/Comment.js
@@ -1,6 +1,7 @@
 // I -- import mongoose
 const mongoose = require("mongoose");
 const { replySchema } = require('./Reply');
+const Joi = require('joi');
 
 // C -- code out the SCHEMA and MODEL for COMMENT and REPLY
 
@@ -16,10 +17,17 @@ const commentSchema = new mongoose.Schema({
 const Comment = mongoose.model("Comment", commentSchema);
 
 const validateComment = (comment) => {
-    
+    const validator = Joi.object({
+       videoID: Joi.string().required(),
+       text: Joi.string().min(4).max(255).required(),
+       likes: Joi.number().default(0),
+       dislikes: Joi.number().default(0),
+    })
+    return validator.validate(comment)
 }
 
 
 // E -- export the MODEL so we can access these instructions elsewhere
 module.exports.Comment = Comment;
 module.exports.commentSchema = commentSchema
+module.exports.validateComment = validateComment
